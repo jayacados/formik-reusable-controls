c5d8b3d350509ef3bccb1e97847be22ec2900009
jayacados <jayacados@gmail.com>
2021-01-20 13:40:12 -0800

    cleanup: formatting

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -1,75 +1,53 @@
-import React from "react";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import FormikControl from "../Formik/FormikControl"
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormikControl from '../Formik/FormikControl';
 
 const EnrollmentForm = () => {
   const dropdownOptions = [
-    { key: "Select your course", value: "" },
-    { key: "React", value: "react" },
-    { key: "Angular", value: "angular" },
-    { key: "Vue", value: "vue" }
+    { key: 'Select your course', value: '' },
+    { key: 'React', value: 'react' },
+    { key: 'Angular', value: 'angular' },
+    { key: 'Vue', value: 'vue' },
   ];
 
   const checkboxOptions = [
-    { key: "HTML", value: "html" },
-    { key: "CSS", value: "css" },
-    { key: "JavaScript", value: "javascript" }
+    { key: 'HTML', value: 'html' },
+    { key: 'CSS', value: 'css' },
+    { key: 'JavaScript', value: 'javascript' },
   ];
 
   const initialValues = {
-    email: "",
-    bio: "",
-    course: "",
+    email: '',
+    bio: '',
+    course: '',
     skills: [],
-    courseDate: null
+    courseDate: null,
   };
 
   const validationSchema = Yup.object({
-    email: Yup.string().email("Invalid email format").required("Required"),
-    bio: Yup.string().required("Required"),
-    course: Yup.string().required("Required"),
-    courseDate: Yup.date().required("Required").nullable()
+    email: Yup.string().email('Invalid email format').required('Required'),
+    bio: Yup.string().required('Required'),
+    course: Yup.string().required('Required'),
+    skills: Yup.array().min(1).required('Required'),
+    courseDate: Yup.date().required('Required').nullable(),
   });
 
-  const onSubmit = (values) => {
-    console.log("Form data", values);
+  const onSubmit = values => {
+    console.log('Form data', values);
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      {(formik) => {
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {formik => {
         return (
           <Form>
-            <FormikControl
-              control="input"
-              type="email"
-              label="Email"
-              name="email"
-            />
-            <FormikControl control="textarea" label="Bio" name="bio" />
-            <FormikControl
-              control="select"
-              label="Course"
-              name="course"
-              options={dropdownOptions}
-            />
-            <FormikControl
-              control="checkbox"
-              label="Your skillset"
-              name="skills"
-              options={checkboxOptions}
-            />
-            <FormikControl
-              control="date"
-              label="Course date"
-              name="courseDate"
-            />
-            <button type="submit" disabled={!formik.isValid}>
+            <FormikControl control='input' type='email' label='Email' name='email' />
+            <FormikControl control='textarea' label='Bio' name='bio' />
+            <FormikControl control='select' label='Course' name='course' options={dropdownOptions} />
+            <FormikControl control='checkbox' label='Your skillset' name='skills' options={checkboxOptions} />
+            <FormikControl control='date' label='Course date' name='courseDate' />
+            <button type='submit' disabled={!formik.isValid}>
               Submit
             </button>
             {/* ========================================================= */}
