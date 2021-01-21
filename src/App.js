7f73f5b17231ac39f5875a205979212786788821
jayacados <jayacados@gmail.com>
2021-01-20 16:47:48 -0800

    comment out code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles.css';
 import FormikContainer from './components/FormikContainer'; // Used to test all reusable components
-import LoginForm from './components/LoginForm';
-import RegistrationForm from './components/RegistrationForm';
-import EnrollmentForm from './components/EnrollmentForm';
+// import LoginForm from './components/LoginForm';
+// import RegistrationForm from './components/RegistrationForm';
+// import EnrollmentForm from './components/EnrollmentForm';
 
 export default function App() {
   return (
